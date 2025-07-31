5d4a3269b27600344142118d1ed92c9ac021fa7a
AkshayBharathan <akshay.devstack@gmail.com>
2025-07-31 16:01:31 +0530

    Fix:Render-Back-end

--- src/Data/Api_EndPoint.jsx
+++ src/Data/Api_EndPoint.jsx
@@ -1,3 +1,3 @@
-export const UserApi= "https://moblemartbackend-3.onrender.com/users"
-export const ProductApi= "https://moblemartbackend-3.onrender.com/products"
-export const NotificationApi= "https://moblemartbackend-3.onrender.com/notifications"
+export const UserApi= "https://moblemartbackend-2.onrender.com/users"
+export const ProductApi= "https://moblemartbackend-2.onrender.com/products"
+export const NotificationApi= "https://moblemartbackend-2.onrender.com/notifications"
